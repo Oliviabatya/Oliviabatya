d06c22617689abb8ef518227bb80dc2e4df5ad95
Oliviabatya <batiahalemu@gmail.com>
2024-11-14 12:17:23 -0500

    lab11

--- lab11.sql
+++ lab11.sql
@@ -1,27 +1,27 @@
---1
+--Question 1
 SELECT title, due_date
 FROM assignments
 WHERE course_id = 'COMP1234'
 
---2
+--Question 2
 SELECT min(due_date)
 FROM assignments
 
---3
+--Question 3
 SELECT max(due_date)
 FROM assignments
 
---4
+--Question 4
 SELECT title, course_id
 FROM assignments
 WHERE due_date = '2024-10-08'
 
---5
+--Question 5
 SELECT title, due_date
 FROM assignments
 WHERE due_date like '2024-10%'
 
--- Question #6
+--Question 6
 SELECT *
 FROM assignments
 WHERE status = 'Completed'
